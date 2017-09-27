b221063fc221cd1de6ef546f1875ebfc6b95c27b
Patrick Browne <pt.browne@gmail.com>
2017-09-27 08:08:43 +0200

    fix: column order was wrong when col number > 10

--- lcf.py
+++ lcf.py
@@ -13,13 +13,18 @@ def parse_metadatum(l):
     # first character is #, second is space
     return multiple_space.split(l[2:], maxsplit=1)
 
+def col_number(n):
+    r = int(n.replace('Column.', '').strip(':'))
+    return r
+
 def parse_lcf(filepath):
     with open(filepath) as f:
         lines = list(f.readlines())
         metadata = dict(parse_metadatum(l) for l in lines if is_comment(l))
         data = [parse_line(l) for l in lines if not is_comment(l)]
     column_metadata = [kv for kv in metadata.items() if kv[0].startswith('Column.')]
-    column_labels = [kv[1].strip() for kv in sorted(column_metadata, key=lambda kv: kv[0])]
+    sorted_cols = sorted(column_metadata, key=lambda kv: col_number(kv[0]))
+    column_labels = [kv[1].strip() for kv in sorted_cols]
     data = pd.DataFrame.from_records(data, index=column_labels[0], columns=column_labels)
     return data
 
